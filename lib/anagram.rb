949d56b7bae6c275dfd93689bdfa64c7c6614ef7
CALVINCE <calvince.ogwel@gmail.com>
2023-02-09 11:22:14 +0300

    completed anagram extra deliverable

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    def initialize(anagram)
+      @anagram = anagram
+    end
+
+    def match(words)
+      words.select do |word|
+        word.chars.sort == @anagram.chars.sort
+      end
+    end
+  end
+
